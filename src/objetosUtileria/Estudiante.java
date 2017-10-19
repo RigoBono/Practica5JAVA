6693fecafcbe1b9cfb3b715e392d07c1018c94e1
RigoBono <arigoberto@gmail.com>
2017-10-19 15:58:37 -0500

    first commit

---
+++ src/objetosUtileria/Estudiante.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetosUtileria;
+
+/**
+ *
+ * @author RigoBono
+ */
+public class Estudiante {
+    
+    private String nombre;
+    private String paterno;
+    private String materno;
+    private int edad;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the paterno
+     */
+    public String getPaterno() {
+        return paterno;
+    }
+
+    /**
+     * @param paterno the paterno to set
+     */
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    /**
+     * @return the materno
+     */
+    public String getMaterno() {
+        return materno;
+    }
+
+    /**
+     * @param materno the materno to set
+     */
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+}
